fe5fe47f416833ea511538fad57211ec08ecb99e
Sebastian <sebastiandavid2929@gmail.com>
2024-09-18 20:05:34 -0500

    Upload Back-End

---
+++ SyncHealthBack/src/main/java/edu/escuelaing/SyncHealthBack/SyncHealthBackApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.SyncHealthBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncHealthBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncHealthBackApplication.class, args);
+	}
+
+}
---
+++ SyncHealthBack/src/test/java/edu/escuelaing/SyncHealthBack/SyncHealthBackApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.SyncHealthBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncHealthBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
